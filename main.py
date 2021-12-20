07d4774770b063de9f1811391245257dfe2fb4c0
Александр <sasha.brezin7262@gmail.com>
2021-12-20 10:16:48 +0300

    Добавлен рассчет физики

--- main.py
+++ main.py
@@ -3,26 +3,33 @@ import pygame as pg
 
 WIDTH = 800 # ширина
 HEIGHT = 800 # высота
-FPS = 60
+FPS = 120
 
 # цвета
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 Yellow = (255, 255, 0)
 
-x = WIDTH / 4
-y = HEIGHT / 4
-x0 = WIDTH // 2
-y0 = HEIGHT // 2
-M0 = 800 #Масса Солнца
+x = WIDTH / 5
+y = HEIGHT / 5
 
 r = 0
 x = 100
 y = 290
 vx = 0.1
-vy = 1.5
+vy = 1.1
 ax = 0
 ay = 0
+m = 10 #масса планеты
+
+#Солнце
+vx0 = 0
+vy0 = 0
+ax0 = 0
+ay0 = 0
+x0 = WIDTH / 2
+y0 = HEIGHT / 2
+M0 = 400
 
 pg.init()
 pg.mixer.init()
@@ -45,13 +52,23 @@ while running:
     ax = M0 * (x0 - x) / r ** 3
     ay = M0 * (y0 - y) / r ** 3
 
+    ax0 = m * (x0 - x) / r ** 3
+    ay0 = m * (y0 - y) / r ** 3
+
+
     vx += ax
     vy += ay
+
+    vx0 += ax0
+    vy0 += ay0
 
     x += vx
     y += vy
 
-    print(x, y)
+    x0 += vx0
+    y0 += vy0
+
+    # print(x, y)
 
     pg.draw.circle(screen, WHITE, (x, y), 6)
     pg.draw.circle(screen, Yellow, (x0, y0), 19)
